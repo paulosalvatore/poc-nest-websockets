0478c060e1e00875b778468c2d7fbb7f967d4578
Paulo Salvatore <salvatoregames@gmail.com>
2021-04-01 01:51:30 -0300

    chore: eslint, prettier and commitlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
     env: {
         node: true,
-        commonjs: true,
-        es2021: true,
+        jest: true,
     },
-    extends: ['airbnb-base', 'eslint:recommended'],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
+    ignorePatterns: ['.eslintrc.js', '*.js'],
     rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-extra-parens': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-shadow': 'off',
         'arrow-parens': 'off',
         eqeqeq: 'error',
         'function-paren-newline': 'off',
-        indent: ['error', 4],
         'linebreak-style': [2, 'unix'],
         'no-console': [
             'error',
@@ -20,18 +34,10 @@ module.exports = {
                 allow: ['info', 'warn', 'error', 'time', 'timeEnd'],
             },
         ],
-        'no-duplicate-imports': 'error',
+        'no-duplicate-imports': 'off',
         'no-extra-parens': 'error',
         'no-return-await': 'error',
-        'no-shadow': [
-            'error',
-            {
-                builtinGlobals: false,
-                hoist: 'functions',
-                allow: [],
-            },
-        ],
-        'operator-linebreak': [2, 'before', { overrides: { '?': 'after' } }],
+        'no-shadow': 'off',
         'import/prefer-default-export': 'off',
     },
 };
